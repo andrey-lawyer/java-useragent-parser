a1a283c186cd59039d8326d732f332367bec4373
Andrey <andrey-1@ukr.net>
2025-01-20 11:10:30 +0200

    removed using database logic

--- src/main/java/com/useragent_parser/controller/UserAgentController.java
+++ src/main/java/com/useragent_parser/controller/UserAgentController.java
@@ -1,10 +1,10 @@
 package com.useragent_parser.controller;
 
 import com.useragent_parser.model.UserAgentInfo;
+import com.useragent_parser.service.UserAgentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-import com.useragent_parser.service.UserAgentService;
 
 @RestController
 public class UserAgentController {
@@ -16,19 +16,15 @@ public class UserAgentController {
         this.userAgentService = userAgentService;
     }
 
-    // Endpoint to parse user agent from the "User-Agent" header
     @CrossOrigin(origins = "*")
     @GetMapping("/api/user-agent/parse")
     public Mono<UserAgentInfo> parseUserAgent(@RequestHeader("User-Agent") String userAgent) {
-        // Cache the result for a GET request
-        return userAgentService.parseUserAgent(userAgent, true);
+        return userAgentService.parseUserAgent(userAgent);
     }
 
-    // New endpoint to parse user agent from request body without caching
     @CrossOrigin(origins = "*")
     @PostMapping("/api/user-agent/parse")
     public Mono<UserAgentInfo> parseUserAgentFromBody(@RequestBody String userAgent) {
-        // Do not cache the result for POST request
-        return userAgentService.parseUserAgent(userAgent, false);
+        return userAgentService.parseUserAgent(userAgent);
     }
 }
